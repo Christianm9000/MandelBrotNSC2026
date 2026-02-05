36e9dd18e9a3871a95f1ac8ff888a45a152342c5
Christianm9000 <christian.mariager9000@gmail.com>
2026-02-05 14:43:32 +0100

    Add mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,19 @@
+"""
+Mandelbrot Set Generator
+Author : Christian Mariager
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+    """
+    Example function .
+    Parameters
+    ----------
+    x : float
+    Input value
+    Returns
+    -------
+    float
+    Output value
+    """
+    pass
+    # TODO : Implement the algorithm
