eeda5329bd1b11897c1e13cd4279c77fdd5ddef9
Christianm9000 <christian.mariager9000@gmail.com>
2026-02-05 15:19:28 +0100

    Implemented mandelbrot_point function which outputs N iteration for a complex input

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,18 +3,37 @@ Mandelbrot Set Generator
 Author : Christian Mariager
 Course : Numerical Scientific Computing 2026
 """
-def f(x):
+
+import numpy as np
+
+def mandelbrot_point(complex_input: complex):
     """
-    Example function .
-    Parameters
-    ----------
-    x : float
-    Input value
-    Returns
-    -------
-    float
-    Output value
+    Docstring for mandelbrot_point
+
+    Description:
+    - Takes a single complex number c
+    - returns iteration count
+    - test with known points (e.g., c=0 should be max_iterations)
     """
+    # Initialize z to 0
+    z_0 = 0 + 0j
+
+    for iteration in range(max_iterations):
+        if abs(z_0) > 2:
+            return iteration
+        z_0 = z_0**2 + complex_input
+
+    # Return the max iteartion count if the point does not escape
+    return max_iterations
+
+def compute_mandelbrot():
     pass
-    # TODO : Implement the algorithm
-    # Here is an extra to do to show i can push to git
+
+if __name__ == "__main__":
+    x_space = [-2.0, 1.0] # Real axis range
+    y_space = [-1.5, 1.5] # Imaginary axis range
+    resolution = 1000 # Number of points along each axis
+    max_iterations = 1000 # Maximum number of iterations to determine if a point escapes
+
+    test_number = 1.5 + -0.2j # Example complex number for testing
+    print(f"Iteration count for {test_number}: {mandelbrot_point(test_number)}")
